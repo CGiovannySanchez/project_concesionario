83eaad96c22cbcfbb41ba2c788f0146888f10014
Giovanny Sanchez <giovanny-sn@hotmail.com>
2024-01-12 00:34:48 -0500

    crear pojo, entity y mapper de marca_coche

---
+++ src/main/java/com/project/concesionario/persitance/entity/MarcaCocheEntity.java
@@ -0,0 +1,23 @@
+package com.project.concesionario.persitance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Entidad de marca coche
+ */
+
+@Getter @Setter
+@Entity
+@Table(name = "marca_coche")
+public class MarcaCocheEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "descripcion")
+    private String description;
+}
+
---
+++ src/main/java/com/project/concesionario/persitance/mapper/IMarcaCocheMapper.java
@@ -0,0 +1,7 @@
+package com.project.concesionario.persitance.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface IMarcaCocheMapper {
+}
